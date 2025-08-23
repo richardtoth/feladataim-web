aa6cabdec23102ae17e8e03342e7cdc96d415bc8
Richard Toth <richardtothhu@gmail.com>
2025-08-23 17:02:38 +0200

    Add CSS import and update dependencies

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <h1>Home</h1>
+    <h1 class="text-5xl text-red">Home</h1>
   </div>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/scripts',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/scripts',
+        '@nuxt/image',
+        '@nuxt/ui',
+    ]
 })
