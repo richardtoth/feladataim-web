d90f21a6d42920ddeee60554964ed885736fc144
Richard Toth <richardtothhu@gmail.com>
2025-08-24 20:38:36 +0200

    Make debug mode configurable via environment variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: {enabled: true},
     css: ['~/assets/css/main.css'],
-    debug: true,
+    debug: process.env.NUXT_DEBUG === 'true',
 
     modules: [
         '@nuxt/eslint',
